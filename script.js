4066e5fd5041b58448f8f9ee25530ae966c13965
Erica Roquemore <enroquemore@gmail.com>
2022-11-18 00:13:51 -0500

    update

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
+// function for logic and prompts
+function generatePassword() {
+    // logic here for prompts
+    
+}
+
+
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
